3532afae128c6f880c44e137a13ce14e16029c26
Ariel Ortega <56314418+Ferrelofilo@users.noreply.github.com>
2020-04-29 19:58:33 -0500

    Proposed models analyzed

--- AnalisisSerieTiempo.R
+++ AnalisisSerieTiempo.R
@@ -199,7 +199,7 @@ autoplot(DNGSP)
 
 # Ahora veremos cómo se comportan la FAC y la FACP
 DFAC <- acf(DNGSP)
-DFACP <- pacf(DNGSP)
+DFACP <- pacf(DNGSP) # Hacer test de significancia para ver si realmente son 0s
 
 ggAcf(DNGSP, lag.max=200)
 ggtsdisplay(DNGSP, lag.max=200)
@@ -209,7 +209,10 @@ ggtsdisplay(DNGSP, lag.max=200)
 # De hecho, con tsclean sale un ARIMA(1,1,0)
 # pero intentaremos con un AR(1)
 autoplot(NGSP)
-model <- Arima(NGSP, order=c(0,1,0)) # Prometedor
+model <- Arima(NGSP, order=c(0,1,0)) # Este pasó :D
+model <- Arima(NGSP, order=c(1,1,0)) # Este sólo no pasa parsimonía
+model <- Arima(NGSP, order=c(1,2,0))
+
 
 model <- Arima(NGSP, order=c(0,1,1)) #Nope
 model <- Arima(NGSP, order=c(0,2,1)) # Prometedor
@@ -267,11 +270,23 @@ checkresiduals(model)
 # De forma visual, parece ser que la varianza no es constante pues
 # se presentan algunos picos que pueden afectar la varianza de los residuales. 
 # Para confirmar, aplicaremos una prueba:
+# En el 2006 parece haber un problema (buscar que pasó) y ponerlo en el escrito
+# Idea de mi amorcito preciosa <3
+
 
 
 # Supuesto 3 (residuos independientes)
 # Prueba de Ljung-Box
 checkresiduals(model)
+Box.test(residuals, lag = 24, type = c("Ljung-Box"))
+# Parecer que a partir de un lag  de 9 se empieza a rechazar independencia
+## PREGUNTA
+# https://stats.stackexchange.com/questions/6455/how-many-lags-to-use-in-the-ljung-box-test-of-a-time-series
+# Potencialmente usar prueba de correlación de atrasos para checar independencia
+# CHECAR 
+# PREGUNTA
+
+
 # Los residuos son independientes
 
 # Supuesto 4 (normalidad)
@@ -280,17 +295,46 @@ checkresiduals(model)
 LI2 <- -2*desv
 LS2 <- 2*desv
 
+desv <- sqrt(var(residuals))
+desv
+
+LEFT <- -2*desv
+RIGHT <- 2*desv
+
+JIJI <- length(residuals[residuals>RIGHT]) + length(residuals[residuals<LEFT])
+JIJI
+JIJI/length(residuals)
+# Se esperaba 5% y es lo que pasó lol
+
+breaks <- seq(from = -10, to = 10, by = 0.5)
+hist(residuals, breaks = breaks)
+# Sí se ve simétrico lol
+
 qqnorm(residuals)
 qqline(residuals)
 checkresiduals(model)
 shapiro.test(residuals)
 lillie.test(x = residuals)
 
+
+
 # Supuesto 5 (no observaciones aberrantes)
 # Prácticamente todas las observaciones deberían estar dentro del intervalo que se extiende 3 desviaciones
 # estándar por arriba y por debajo de la media, la cual esperamos que sea 0.
 LI3 <- -3*desv
 LS3 <- 3*desv
+
+desv <- sqrt(var(residuals))
+desv
+
+LEFT <- -3*desv
+RIGHT <- 3*desv
+
+JIJI <- length(residuals[residuals>RIGHT]) + length(residuals[residuals<LEFT])
+JIJI
+JIJI/length(residuals)
+# 3% que era más o menos lo que esperábamos
+
 
 
 plot(res_autoarima, ylim=c(-2*10^(-3),6*10^(-4)))
@@ -301,10 +345,11 @@ abline(h=LI3, col="blue")
 abline(h=LS3, col="blue")
 
 
+
+
 # Supuesto 6 (parsimonía)
 # Ver con un 95% de confianza que todos los parámetros sean diferentes de 0.
 # En este caso no hay parámetros ya que el modelo es I(1).
-
 
 # Supuesto 7 (modelo admisible)
 # Verificar que los parámetros se encuentren dentro de las regiones admisibles correspondientes.
@@ -319,5 +364,197 @@ abline(h=LS3, col="blue")
 
 ####################### Pronósticos #######################
 # auto.arima
-autoarima_pronostico <-forecast(autoarima, h = 3) # Duda con h = 3
+autoarima_pronostico <-forecast(autoarima, h = 5) # Duda con h = 3
 autoplot(autoarima_pronostico)
+
+#https://stats.stackexchange.com/questions/333092/why-i-get-the-same-predict-value-in-arima-model
+autoarima_pronostico
+
+# Los pronósticos parecen estar bien 
+# Te amo <3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#########################################################################################################
+# Supuesto 1 (media cero)
+#########################################################################################################
+# Debemos verificar que el valor absoluto del cociente sea menor que dos para decir que no hay evidencia de que 
+# la media del proceso sea diferente de 0. 
+media <- mean(residuals)
+media
+desv <- sqrt(var(residuals))
+desv
+
+N <- length(residuals)
+p <- 0
+d <- 1
+q <- 0
+cociente <- (sqrt(N-d-p)) * (media/desv)
+abs(cociente)
+
+## ARIMA(1,1,0) pasa
+# Como el valor absoluto del conciente es menor que 2, entonces podemos decir que no hay evidencia
+# suficiente para afirmar que la media del proceso es distinta de cero.
+
+
+#########################################################################################################
+# Supuesto 2 (varianza constante)
+#########################################################################################################
+# Observamos de manera visual si la varianza parece ser constante o no
+checkresiduals(model)
+# De forma visual, parece ser que la varianza no es constante pues
+# se presentan algunos picos que pueden afectar la varianza de los residuales. 
+# Para confirmar, aplicaremos una prueba:
+# En el 2006 parece haber un problema (buscar que pasó) y ponerlo en el escrito
+# Idea de mi amorcito preciosa <3
+# Checar de nuevo xd
+
+
+# Supuesto 3 (residuos independientes)
+# Prueba de Ljung-Box
+checkresiduals(model)
+Box.test(residuals, lag = 9, type = c("Ljung-Box"))
+
+# Test de significancia para ver si autocorrelaciones son 
+# significativamente distintas de 0?
+
+# Parecer que a partir de un lag  de 9 se empieza a rechazar independencia
+## PREGUNTA
+# https://stats.stackexchange.com/questions/6455/how-many-lags-to-use-in-the-ljung-box-test-of-a-time-series
+# Potencialmente usar prueba de correlación de atrasos para checar independencia
+# CHECAR 
+# PREGUNTA
+
+
+# Los residuos son independientes
+
+# Supuesto 4 (normalidad)
+# Verificar que aprox. el 95% de las observaciones se encuentren dentro del intervalo que se extiende 2 
+# desviaciones estándar por arriba y por debajo de la media, la cual esperamos que sea 0.
+LI2 <- -2*desv
+LS2 <- 2*desv
+
+desv <- sqrt(var(residuals))
+desv
+
+LEFT <- -2*desv
+RIGHT <- 2*desv
+
+JIJI <- length(residuals[residuals>RIGHT]) + length(residuals[residuals<LEFT])
+JIJI
+JIJI/length(residuals)
+# Se esperaba 5% y es lo que pasó lol
+
+breaks <- seq(from = -10, to = 10, by = 0.5)
+hist(residuals, breaks = breaks)
+# Sí se ve simétrico lol
+
+qqnorm(residuals)
+qqline(residuals)
+checkresiduals(model)
+shapiro.test(residuals)
+lillie.test(x = residuals)
+
+
+
+# Supuesto 5 (no observaciones aberrantes)
+# Prácticamente todas las observaciones deberían estar dentro del intervalo que se extiende 3 desviaciones
+# estándar por arriba y por debajo de la media, la cual esperamos que sea 0.
+LI3 <- -3*desv
+LS3 <- 3*desv
+
+desv <- sqrt(var(residuals))
+desv
+
+LEFT <- -3*desv
+RIGHT <- 3*desv
+
+JIJI <- length(residuals[residuals>RIGHT]) + length(residuals[residuals<LEFT])
+JIJI
+JIJI/length(residuals)
+# 3% que era más o menos lo que esperábamos
+
+
+
+plot(res_autoarima, ylim=c(-2*10^(-3),6*10^(-4)))
+plot(res_autoarima)
+abline(h=LS2, col="red")
+abline(h=LI2, col="red")
+abline(h=LI3, col="blue")
+abline(h=LS3, col="blue")
+
+
+
+
+# Supuesto 6 (parsimonía)
+# Ver con un 95% de confianza que todos los parámetros sean diferentes de 0.
+# En este caso no hay parámetros ya que el modelo es I(1).
+meanie <- model$coef
+se <- sqrt(model$var.coef)
+
+meanie - (2)*se
+meanie + (2)*se
+
+# Parece ser que el supuesto de parsimonía no se cumple
+# Parece no ser tan grave, tiene sentido que sea modelado por un ARI(1,1)
+# Justificar bien
+
+
+
+# Supuesto 7 (modelo admisible)
+# Verificar que los parámetros se encuentren dentro de las regiones admisibles correspondientes.
+# En este caso no hay parámetros ya que el modelo es I(1).
+(abs(meanie)<1)
+
+
+# Supuesto 8 (modelo estable)
+# Calculamos las correlaciones entre pares para ver que sean bajas.
+
+# Como sólo hay un parámetro, no hay autocorrelaciones entre parámetros
+
+
+
+####################### Pronósticos #######################
+# auto.arima
+autoarima_pronostico <-forecast(autoarima, h = 5) # Duda con h = 3
+autoplot(autoarima_pronostico)
+
+#https://stats.stackexchange.com/questions/333092/why-i-get-the-same-predict-value-in-arima-model
+autoarima_pronostico
+
+# Los pronósticos parecen estar bien 
+# Te amo <3
