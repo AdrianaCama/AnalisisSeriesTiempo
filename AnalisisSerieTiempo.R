4a25ea56434a761af9c5e7692c106c0ba1fca6ab
Ariel Ortega <56314418+Ferrelofilo@users.noreply.github.com>
2020-05-03 22:52:06 -0500

    Tabla de correlaciones

--- AnalisisSerieTiempo.R
+++ AnalisisSerieTiempo.R
@@ -40,6 +40,7 @@ datos <- read.csv("Henry_Hub_Natural_Gas_Spot_Price.csv", header = TRUE)
 datos <- datos[rev(rownames(datos)),]
 NGSP <- ts(datos[,2], start=1997, freq=12)
 
+
 # Usar estos comandos para verificar el directorio actual y cambiarlo, de ser necesario
 # getwd()
 # setwd("C:/Users/orteg/OneDrive/Documents/GitHub/AnalisisSeriesTiempo")
